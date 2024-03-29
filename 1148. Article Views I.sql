4bf0b1c50dda98fef0577deec606b5a153a0128b
Krishnan <krishnan.spu@gmail.com>
2024-03-29 23:06:24 +0530

    feat: added few more solutions

---
+++ 1148. Article Views I.sql
@@ -0,0 +1,28 @@
+Table: Views
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+
+Write a solution to find all the authors that viewed at least one of their own articles.
+Return the result table sorted by id in ascending order.
+The result format is in the following example.
+
+
+Solution:
+
+SELECT DISTINCT (author_id) AS id
+FROM VIEWS v
+WHERE author_id IN (
+		SELECT viewer_id
+		FROM VIEWS v
+		WHERE v.author_id = v.viewer_id
+		)
+ORDER BY 1 ASC;
